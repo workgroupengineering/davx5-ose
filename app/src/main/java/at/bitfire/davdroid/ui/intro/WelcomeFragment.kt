a864ac87f9a931172d2927b258986f3e3f5f4fa5
Patrick Lang <patrick.lang85@gmail.com>
2021-04-25 12:54:36 +0200

    Added intro animation and cleaned up intro_welcome.xml

--- app/src/main/java/at/bitfire/davdroid/ui/intro/WelcomeFragment.kt
+++ app/src/main/java/at/bitfire/davdroid/ui/intro/WelcomeFragment.kt
@@ -5,14 +5,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import at.bitfire.davdroid.R
 import at.bitfire.davdroid.settings.SettingsManager
 
+
 class WelcomeFragment: Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-            inflater.inflate(R.layout.intro_welcome, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val  v = inflater.inflate(R.layout.intro_welcome, container, false)
+
+        val logo: ImageView = v.findViewById(R.id.logo)
+        val yourdata: TextView = v.findViewById(R.id.yourDataYourChoice)
+        val takecontrol: TextView = v.findViewById(R.id.takeControl)
+
+        logo.apply {
+            animation = AnimationUtils.loadAnimation(this.context, R.anim.fadein)
+            animation.startOffset = 0
+            animation.start()
+        }
+        yourdata.apply {
+            animation = AnimationUtils.loadAnimation(this.context, R.anim.fadein)
+            animation.startOffset = 1000
+            animation.start()
+        }
+        takecontrol.apply {
+            animation = AnimationUtils.loadAnimation(this.context, R.anim.fadein)
+            animation.startOffset = 2000
+            animation.start()
+        }
+
+        return v
+    }
 
 
     class Factory : IIntroFragmentFactory {
